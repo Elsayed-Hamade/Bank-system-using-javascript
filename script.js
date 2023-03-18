12e16d4ceb6acc25cf92f53983e1b0ca44f33ab6
Elsayed Hamada <elsayed.7work@gmail.com>
2023-03-18 14:40:56 +0200

    Simplify js

--- script.js
+++ script.js
@@ -11,66 +11,62 @@ const loanPageBtn = document.querySelector(".loan-btn");
 const movements = document.querySelector(".movements-container");
 const transfer = document.querySelector(".transfer-container");
 const loan = document.querySelector(".loan-container");
-const profileBtn = document.querySelector(".profile-icon")
-const profile = document.querySelector(".profile-container")
-const pagesBtn = document.querySelector(".pages-btn")
+const profileBtn = document.querySelector(".profile-icon");
+const profile = document.querySelector(".profile-container");
+const pagesBtn = document.querySelector(".pages-btn");
 /* STYLING FUNCTIONALITY */
 //Sign & Log
+
+const changeClass = (div1, div2, class1, class2, action) => {
+  if (action === "add") {
+    div1.classList.add(class1);
+    div2.classList.add(class2);
+  } else if (action === "remove") {
+    div1.classList.remove(class1);
+    div2.classList.remove(class2);
+  }
+};
+
 logBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  logBtn.classList.add("switch-btn-active");
-  signBtn.classList.remove("switch-btn-active");
-  logInForm.classList.remove("inactive");
-  signUpForm.classList.add("inactive");
+  changeClass(logBtn, signUpForm, "switch-btn-active", "inactive", "add")
+  changeClass(signBtn, logInForm, "switch-btn-active", "inactive", "remove")
   logSignBtn.textContent = "Log In";
 });
 
 signBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  signBtn.classList.add("switch-btn-active");
-  logBtn.classList.remove("switch-btn-active");
-  logInForm.classList.add("inactive");
-  signUpForm.classList.remove("inactive");
+  changeClass(signBtn, logInForm, "switch-btn-active", "inactive", "add")
+  changeClass(logBtn, signUpForm, "switch-btn-active", "inactive", "remove")
   logSignBtn.textContent = "Sign Up";
 });
 
 // Pages
-const addClasses = ()=>{
-  transfer.classList.add("inactive");
-  movements.classList.add("inactive");
-}
-
 balancePageBtn.addEventListener("click", () => {
   balancePageBtn.classList.add("btn-active");
-  transferPageBtn.classList.remove("btn-active");
-  loanPageBtn.classList.remove("btn-active");
-  loan.classList.add("inactive");
-  transfer.classList.add("inactive");
   movements.classList.remove("inactive");
+  changeClass(transferPageBtn, loanPageBtn, "btn-inactive", "btn-inactive", "remove")
+  changeClass(transfer, loan, "inactive", "inactive", "add")
 });
 
 transferPageBtn.addEventListener("click", () => {
-  balancePageBtn.classList.remove("btn-active");
+  changeClass(balancePageBtn, loanPageBtn, "btn-active", "btn-active", "remove")
+  changeClass(movements, loan, "inactive", "inactive", "add")
   transferPageBtn.classList.add("btn-active");
-  loanPageBtn.classList.remove("btn-active");
-  movements.classList.add("inactive");
-  loan.classList.add("inactive");
   transfer.classList.remove("inactive");
 });
 
 loanPageBtn.addEventListener("click", () => {
-  balancePageBtn.classList.remove("btn-active");
-  transferPageBtn.classList.remove("btn-active");
+  changeClass(transferPageBtn, balancePageBtn, "btn-active", "btn-active", "remove")
+  changeClass(transfer, movements, "inactive", "inactive", "add")
   loanPageBtn.classList.add("btn-active");
-  addClasses()
   loan.classList.remove("inactive");
 });
 
-profileBtn.addEventListener("click", ()=>{
-  pagesBtn.classList.add("inactive")
-  loan.classList.add("inactive")
-  addClasses()
-  profile.classList.remove("inactive")
-})
+profileBtn.addEventListener("click", () => {
+  changeClass(pagesBtn, loan, "inactive", "inactive", "add")
+  changeClass(transfer, movements, "inactive", "inactive", "add")
+  profile.classList.remove("inactive");
+});
 
 /* BANK FUNCTIONALITY */
